abac4f6f27b9b9b638b617d381981bbfeb75fcfd
vincentYoung <vinni.xander@gmail.com>
2023-05-11 08:57:42 -0600

    build form submission, api call & image display func

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,33 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector("form") as HTMLFormElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  // extract data from form by instantiating a new FormData object using our form as an input - provides us with a data strcuture that behaves like a js Map
+  // could also just access prompt input directly
+  const formData = new FormData(form);
+  // grb prompt value from form using get method available on FormData instances
+  const prompt = formData.get("prompt");
+
+  // make request to our api using fetch api which returns a promise (so we need to await resolution of promise)
+  const response = await fetch("http://localhost:8080/imagine", {
+    // make sure it is using http method of post because thats how we built our api
+    method: "POST",
+    // because we are dealing
+    headers: {
+      // because we are dealing with a json api
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt,
+    }),
+  });
+  // access image url from response
+  const { image } = await response.json();
+  // insert image in ui
+  const result = document.querySelector("#result") as HTMLDivElement;
+  // the width matches css max width and since in server we are req aspect-ratio of 1/1 and also specifying this in css I guess we only need width here
+  result.innerHTML = `<img src="${image}" width="512"/>`;
+});
